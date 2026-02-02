b2aec5776cea4912f6655968fd237d6428324bfa
Juan Jose Cardona Gomez <juanjo-cardona@hotmail.com>
2026-02-01 21:22:42 -0500

    Modulos de productos - 05

---
+++ src/data/productosLaLola.js
@@ -0,0 +1,4 @@
+export const productos = [
+  { id: 1, name: 'Cacao', img: '/la-lola/cacao.png', longDesc: 'Cacao en polvo puro 100%' },
+  { id: 2, name: 'Lechenela', img: '/la-lola/lechenela.png', longDesc: 'Lechenela tradicional' }
+];
