67214ce8113cb332689fc9d4ea540ab3572e0e8d
Juan Jose Cardona Gomez <juanjo-cardona@hotmail.com>
2026-02-03 10:16:39 -0500

    Integracion boton whatsapp - 08

--- src/data/productosLaLola.js
+++ src/data/productosLaLola.js
@@ -1,4 +1,6 @@
-export const productos = [
-  { id: 1, name: 'Cacao', img: '/la-lola/cacao.png', longDesc: 'Cacao en polvo puro 100%' },
-  { id: 2, name: 'Lechenela', img: '/la-lola/lechenela.png', longDesc: 'Lechenela tradicional' }
+export const listaProductosLaLola = [
+    { id: 1, name: 'Cacao', img: '/la-lola/cacao.png', longDesc: 'Cacao en polvo puro 100%' },
+    { id: 2, name: 'Lechenela', img: '/la-lola/lechenela.png', longDesc: 'Lechenela tradicional' },
+    { id: 3, name: 'Panela en polvo', img: '/la-lola/panela.png', longDesc: 'Panela en polvo 100%' },
+    { id: 4, name: 'Leche en polvo', img: '/la-lola/leche.png', longDesc: 'Leche en polvo tradicional' }
 ];
---
+++ src/data/productosLaRueda.js
@@ -0,0 +1,9 @@
+export const listaProductosLaRueda = [
+    { id: 1, name: 'Queso Semi Maduro', img: '/la-rueda/queso-semi-maduro.png', longDesc: 'Queso semi maduro de alta calidad' },
+    { id: 2, name: 'Queso Maduro', img: '/la-rueda/queso-maduro.png', longDesc: 'Queso maduro de alta calidad' },
+    { id: 3, name: 'Queso Jalapeño', img: '/la-rueda/queso-jalapeño.png', longDesc: 'Queso con sabor jalapeño' },
+    { id: 4, name: 'Queso Tomillo', img: '/la-rueda/queso-tomillo.png', longDesc: 'Queso con sabor tomillo' },
+    { id: 5, name: 'Queso Pimienta', img: '/la-rueda/queso-pimienta.png', longDesc: 'Queso con sabor pimienta putumayo' },
+    { id: 6, name: 'Queso Trufa de Italia', img: '/la-rueda/queso-trufa-italia.png', longDesc: 'Queso con sabor trufa de Italia' },
+    { id: 7, name: 'Queso Finas hierbas', img: '/la-rueda/queso-finas-hierbas.png', longDesc: 'Queso con sabor finas hierbas' }
+];
